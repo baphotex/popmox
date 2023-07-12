e18334a034bb79001ad85cb22556d8fabd943aab
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-12 17:58:39 -0500

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -2,12 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for, request # New i
 
 post_pages = Blueprint("posts", __name__)
 
-
 @post_pages.get("/post/<string:title>")
-def display_post(title: str):
-    return "Display post page."
-
-
+def display_post(title):
+    content = "..." # How do we get the content?
+    return render_template("post.html", title=title, content=content)
+    
 @post_pages.route("/post/", methods=["GET", "POST"])
 def create_post():
     if request.method == "POST":
