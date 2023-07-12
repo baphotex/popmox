127987fc03c3471dacb91a30ca38e012a3429ba2
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-12 18:00:23 -0500

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -6,9 +6,11 @@ post_pages = Blueprint("posts", __name__)
 
 @post_pages.get("/post/<string:title>")
 def display_post(title):
-    content = "..." # How do we get the content?
-    return render_template("post.html", title=title, content=content)
-
+    with Session(current_app.engine) as session:
+        statement = select(Post).where(Post.title == title)
+        post = session.exec(statement).first()
+        return render_template("post.html", title=title, content=post.content)
+        
 @post_pages.route("/post/", methods=["GET", "POST"])
 def create_post():
     if request.method == "POST":
