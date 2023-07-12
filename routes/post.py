19219544ea2e66946a2703269738d45de6d5ffb5
baphotex <136928692+baphotex@users.noreply.github.com>
2023-07-12 17:59:44 -0500

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, request # New imports added
+from flask import Blueprint, render_template, redirect, url_for, request, current_app # New imports added
+from sqlmodel import Session, select
+from models.post import Post
 
 post_pages = Blueprint("posts", __name__)
 
@@ -6,13 +8,15 @@ post_pages = Blueprint("posts", __name__)
 def display_post(title):
     content = "..." # How do we get the content?
     return render_template("post.html", title=title, content=content)
-    
+
 @post_pages.route("/post/", methods=["GET", "POST"])
 def create_post():
     if request.method == "POST":
         title = request.form.get("title")
         content = request.form.get("content")
-        # TODO: We can create the post in our database here
-        return redirect(url_for(".display_post", title=title))
+        with Session(current_app.engine) as session:
+            session.add(Post(title=title, content=content))
+            session.commit()
+        return redirect(url_for("display_post", title=title))
     return render_template("new_post.html")
-    
+
